10c0d40e010b2905802e2a9cad3e294853c42789
Ahkeem Lang <langahkeem@gmail.com>
2016-06-24 10:52:22 -0500

    Made a few changes to my work, that was required of me by feedback

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,68 +1,70 @@
 
-class Player
-  attr_accessor :name
-  def initialize(name)
-    @name = name
-  end
-  def pick_a_number
-    print "<Choose a number:>"
-    gets.chomp.to_i
-  end
-  def same_number(attemts)
-
-  end
-end
-
-
-class Game
-  def welcome
-  puts  "Welcome to my guessing game! Do you have what it takes to guess right?! Would you like to play (y)es or (n)o"
-  gets.chomp
-  end
-  def grab_name
-    print "<What's your name>: "
-    gets.chomp
-  end
-  def win_logic(player_guess, player)
-    turns = 0
-    recorded_guess = []
-    recorded_guess << player_guess
-    loop do
-      puts "Turn #{turns}"
-      if turns == MAX_PLAYER_ATTEMPTS
-        puts "Sorry... game is over!"
-        break
-      elsif player_guess < SECRET_NUMBER
-        puts "Your guess is too Low."
-        player_guess = player.pick_a_number
-         if recorded_guess.include?(player_guess)
-             puts "You picked the same number!"
-         end
-      elsif player_guess > SECRET_NUMBER
-        puts "Your guess is too High."
-        player_guess = player.pick_a_number
-        if recorded_guess.include?(player_guess)
-            puts "You picked the same number!"
-        end
-      elsif player_guess == same
-        puts "You guessed the same number?!"
-      elsif player_guess == SECRET_NUMBER
-      puts "You won! That's correct!"
-      exit
+    class Player
+      attr_accessor :name
+      def initialize(name)
+        @name = name
       end
-      turns += 1
+
+      def pick_a_number
+        print "<Choose a number:>"
+        gets.chomp.to_i
+      end
+
     end
-  end
 
-  def start
-    welcome
-    name = grab_name
-    player = Player.new(name)
-    pick_a_number = player.pick_a_number
-    win_logic(pick_a_number, player)
 
-  end
-end
+    class Game
+      def welcome
+        puts  "Welcome to my guessing game! Do you have what it takes to guess right?! Would you like to play (y)es or (n)o"
+        gets.chomp
+      end
+
+      def grab_name
+        print "<What's your name>: "
+        gets.chomp
+      end
+
+      def win_logic(player_guess, player)
+        turns = 0
+        recorded_guess = []
+        recorded_guess << player_guess
+        loop do
+          puts "Turn #{turns}"
+          if turns == MAX_PLAYER_ATTEMPTS
+            puts "Sorry... game is over!"
+            break
+          elsif player_guess < SECRET_NUMBER
+            puts "Your guess is too Low."
+            player_guess = player.pick_a_number
+             if recorded_guess.include?(player_guess)
+                 puts "You picked the same number!"
+             end
+
+          elsif player_guess > SECRET_NUMBER
+            puts "Your guess is too High."
+            player_guess = player.pick_a_number
+            if recorded_guess.include?(player_guess)
+                puts "You picked the same number!"
+            end
+
+          elsif player_guess == same
+            puts "You guessed the same number?!"
+          elsif player_guess == SECRET_NUMBER
+          puts "You won! That's correct!"
+          exit
+          end
+          turns += 1
+        end
+      end
+
+      def start
+        welcome
+        name = grab_name
+        player = Player.new(name)
+        pick_a_number = player.pick_a_number
+        win_logic(pick_a_number, player)
+      end
+    end
 
 MAX_PLAYER_ATTEMPTS = 5
 SECRET_NUMBER = rand(1..100)
